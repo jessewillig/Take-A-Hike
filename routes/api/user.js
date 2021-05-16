cb8c7aae93b0e1bc0665b9b6963d2bc0fe904f4e
Jasmine Kay <jasminekay505@gmail.com>
2021-05-16 15:45:17 +0800

    Create a new user works!

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const userController = require("../../controllers/userController");
 const db = require("../../models");
 const bcrypt = require("bcrypt");
 
@@ -13,11 +12,62 @@ router.post('/', async (req, res) => {
         req.session.save(()=> { 
             req.session.user_id = userData._id;
             req.session.logged_in = true;
-            res.status(200).json(userData);
+            res.status(200).json( { 
+                user_id: userData._id,
+                username: userData.username
+            });
         })
         
 
     } catch (err) {
         console.error(err);
+        res.status(400).json(err);
     }
-})
+});
+
+router.post('/login', async(req,res) => { 
+    try {
+        const userData = await db.User.findOne({ email: req.body.email });
+
+        if (!userData) { 
+            res
+                .status(400)
+                .json({ message: "Incorrect eamil or password, please try again."});
+                return;
+        }
+
+        const validPassword = await bcrypt.compare(
+            req.body.password,
+            userData.password
+        );
+        
+        if (!validPassword) { 
+            res
+            .status(400)
+            .json({ message: "Incorrect email or password, please try again. " });
+            return;
+        }
+
+        req.session.save(() => { 
+            req.sesssion.user_id = userData._id;
+            req.session.logged_in = true;
+
+            res.json({ user: {username: userData.username, user_id: userData._id}, message: "You are no loggedin!" })
+        })
+
+    } catch (err) {
+        
+    }
+});
+
+router.post('/logout', (req, res) => { 
+    if(req.sessioin.logged_in) { 
+        req.session.destroy(() => { 
+            res.status(204).end();
+        }); 
+    } else { 
+        res.statue(404).end();
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,47 +1,51 @@
+//Import dependencies
 require('dotenv').config();
 const express = require("express");
+const path = require("path");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 const mongoose = require("mongoose");
 const router = require("./routes");
 
+//Set up app and store session
+const PORT = process.env.PORT || 3001;
 const app = express();
-const store = new MongoDBStore({ 
-  uri: process.env.MONGODB_URI || "mongodb://localhost/hikes",
+const store = new MongoDBStore({
+  uri: process.env.MONGODB_URI || "mongodb://localhost/Hike",
   collection: "sessions"
 });
 
-store.on("error", (error)=> {
+store.on("error", (error) => {
   console.log(error);
 });
 
 app.use(session({
   secret: "Super secret secret",
-  cookie: { 
+  cookie: {
     //Cookie lasts one day
-    maxAge: 1000 * 60 * 60 *24
-  }, 
-  stone: store,
+    maxAge: 1000 * 60 * 60 * 24
+  },
+  store: store,
   saveUninitialized: true,
   resave: true
 }))
 
+//Parse app body as json
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(router);
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-  );
+  process.env.MONGODB_URI || 'mongodb://localhost/Hike',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(PORT, function () {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
-  
