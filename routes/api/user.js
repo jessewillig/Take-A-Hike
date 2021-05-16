0e0c7bd29898539d395fb2db22a61e57277edbab
Jasmine Kay <jasminekay505@gmail.com>
2021-05-16 10:57:04 +0800

    setting up server.js for express session

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,4 +1,16 @@
 const router = require("express").Router();
 const userController = require("../../controllers/userController");
+const db = require("../../models");
 
- 
+router.post('/', async (req, res) => {
+    try {
+
+        
+        const userData = await db.User.create(req.body);
+
+        res.status(200).json(userData);
+
+    } catch (err) {
+        console.error(err);
+    }
+})
--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 const express = require("express");
-
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const router = require("./routes");
 
 const app = express();
+const store = new MongoDBStore({ 
+  uri: process.env.MONGODB_URI || "mongodb://localhost/hikes",
+  collection: "sessions"
+});
+
+store.on("error", (error)=> {
+  console.log(error);
+});
+
+app.use(session({
+  secret: "Super secret secret",
+  cookie: { 
+    //Cookie lasts one day
+    maxAge: 1000 * 60 * 60 *24
+  }, 
+  stone: store,
+  saveUninitialized: true,
+  resave: true
+}))
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(router);
 
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/',
