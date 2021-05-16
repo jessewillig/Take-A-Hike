c33d7e80e9e1df31ce46ac25e4bfb4dcaa4bf6f5
Jasmine Kay <jasminekay505@gmail.com>
2021-05-16 11:06:41 +0800

    setting up user authentication

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,14 +1,21 @@
 const router = require("express").Router();
 const userController = require("../../controllers/userController");
 const db = require("../../models");
+const bcrypt = require("bcrypt");
 
 router.post('/', async (req, res) => {
     try {
+        const newUser = req.body;
+        //hash password
+        newUser.password = await bcrypt.hash(req.body.password, 10);
+        const userData = await db.User.create(newUser);
 
+        req.session.save(()=> { 
+            req.session.user_id = userData._id;
+            req.session.logged_in = true;
+            res.status(200).json(userData);
+        })
         
-        const userData = await db.User.create(req.body);
-
-        res.status(200).json(userData);
 
     } catch (err) {
         console.error(err);
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
