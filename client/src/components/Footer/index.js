4fe0312c824a7b63383bc882c74c23e9fb6b24a4
Ashley Whaley <73979892+AshleyWhaley@users.noreply.github.com>
2021-05-19 20:53:30 -0700

    started footer

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+    return (
+        <div className="">
+            <p>Copyright &copy; 2021 Take-5 Group. All Rights Reserved</p>
+        </div>
+    )
+}
+
+export default Footer;
