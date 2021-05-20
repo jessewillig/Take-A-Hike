68aa7e4d01fe8c5bfb8de5ff2d3ba4c1f63d2c17
Ashley Whaley <73979892+AshleyWhaley@users.noreply.github.com>
2021-05-19 22:40:28 -0700

    started header and added forestjpg

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+Import trees from "../../assets/forest.jpg"
+import "./style.css";
+
+function Header() {
+    return (
+        <div className="">
+            <p>Take a Hike</p>
+        </div>
+    )
+}
