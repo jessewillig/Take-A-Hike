2a482170d1681a55e057529a9df1514f81d88cd2
Jesse Willig <jessewillig@outlook.com>
2021-05-21 14:26:17 -0700

    small syntax fix

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const User = require("./user");
 
 module.exports = { 
     User: require("./user"),
-    Comment: require("./comment"),
-    Hike: require("./hike")
+    Comment: require("./Comment"),
+    Hike: require("./Hike")
 };
 
