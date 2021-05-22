0fe4dbf84a7a8d7d6541fe8e01186007051ee492
Ashley Whaley <73979892+AshleyWhaley@users.noreply.github.com>
2021-05-21 22:46:50 -0700

    added uikit style to search bar

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -9,7 +9,7 @@ function Header() {
            <iframe src="https://giphy.com/embed/gqh8uVtwvemTm" title="I hope we dont get in trouble" width="600" height="400" frameBorder="0"  allowFullScreen></iframe>
         </div>
            
-            <p class="uk-animation-fade">Take a Hike</p>
+            <p class="uk-animation-fade uk-heading-large uk-heading-divider">Take a Hike</p>
         </div>
     )
 }
--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import "./style.css";
 const axios = require('axios');
 const YELP_API_KEY = `Mw30ITWflSolse_YhQacZ7frFs0bKAwh-wteoRdVU4o3S9bfSkLyRsUsCToHzGSzZEvalTsvVasyid3MUq_HdOf3RGI-GUcamb557Pe7CGC5CsDVlmESpAApUgmiYHYx`
 
@@ -33,20 +34,24 @@ export function SearchBar(props) {
 
     return (
       <div className="wrap">
-        <form onSubmit={submit}>
+        <div class="uk-card uk-card-default uk-card-hover uk-width-1-5@m">
+          <h3 class="uk-card-title">Search For Hike.</h3>
+        <form className="uk-search uk-search-default" onSubmit={submit}>
             <div className="search-bar-component">
-                <p>
-                    <button>Search</button>
-                </p>
                 <div className="search-location">
                     <input
+                           class="uk-search-input"
                            onChange={(e) => setLocation(e.target.value)}
-                           type="text"
+                           type="search"
                            value={location}
                            placeholder="Location"/>
                 </div>
+                <p>
+                    <button className="uk-button uk-button-primary uk-button-small">Search</button>
+                </p>
             </div>
         </form>
+        </div>
         <div className="search-result">
           <h2>
             Trail Name: {JSON.stringify(trailName)}
