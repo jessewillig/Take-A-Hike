dc616c8e1645151df92c5fdb7435b19132b5f899
Jesse Willig <jessewillig@outlook.com>
2021-05-23 19:16:48 -0700

    fixed hike.js route

--- routes/api/hike.js
+++ routes/api/hike.js
@@ -1,40 +1,18 @@
 const router = require("express").Router();
-const { Hike, Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
+const hikeController = require("../../controllers/hikeController")
 
+// Matches with "/api/hikes"
+router
+  .route("/")
+  .get(hikeController.findAll)
+  .post(hikeController.create);
 
-router.post("/", withAuth, async (req, res) => {
-    try {
-        const newHike = await Hike.create({
-            text: req.body.newComment,
-            date: req.body.date,
-            rating: req.body.newRating,
-            author: req.session.user_id,
-        });
-        res.status(200).json(newHike);
-    } catch (err) {
-        res.status(500).json(err)
-    }
-
-});
-
-router.get("/comment/:id", async (req, res) => {
-    try {
-        const commentData = await Comment.findByPk(req.params.id, {
-            include: [
-                {
-                    model: Hike
-                },
-            ],
-        });
-        const comment = commentData.get({ plain: true });
-
-        res.render("review", {
-            ...comment,
-            logged_in: res.session.logged_in
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+// Matches with "/api/hikes/:id"
+router
+  .route("/:id")
+  .get(hikeController.findById)
+  .put(hikeController.update)
+  .delete(hikeController.remove);
 
 module.exports = router;
