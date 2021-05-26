a9580a430007e6327a2c91d7ff62a6db65df18ec
Ashley Whaley <73979892+AshleyWhaley@users.noreply.github.com>
2021-05-25 19:31:33 -0700

    removed commas

---
+++ client/src/pages/Profile/Profile.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import Header, from "../../components/Jumbotron";
+import Nav, from "../../components/Nav";
+import Hike, from "../../components/Hike";
+import Footer, from "../../components/Footer";
+
+
