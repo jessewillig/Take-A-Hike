0d47b1819eba78628529d35b82315ddc59ce08a8
Nate J <jamis.walsh@gmail.com>
2021-05-31 13:26:45 -0700

    Styling and display fixes

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -24,6 +24,7 @@ export function SearchBar(props) {
           }
         )
         .then(response => {  
+            console.log(response.data.businesses[0]);
             setTrailResults(
               response.data.businesses.map(business => ({
                 name: business.name.replace(/['"]+/g, ''),
@@ -65,18 +66,12 @@ export function SearchBar(props) {
           <div className="uk-card uk-card-default uk-card-hover uk-width-1-4@m uk-margin-left">
             <div className="search-result">
               <h2>
-                Trail Name:{" "}
-
-                {JSON.stringify(trailResults[0].name)}
+                Trail Name: {trailResults[0].name}
               </h2>
-
               <p>
-                Location:{" "}
-                {JSON.stringify([
-                  trailResults.city,
-                  trailResults.state,
-                ]).replace(/['"]+/g, "")}
+                Location: {trailResults[0].city + `, ` + trailResults[0].state}
               </p>
+              <img src={trailResults[0].image_url} height="150px" width="150px" alt="Yelp" uk-img></img>
             </div>
           </div>
         </div>
