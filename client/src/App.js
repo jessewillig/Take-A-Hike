1dbab672e07ea2a99f0073ff0e8c8208ad3b0e9e
Ashley Whaley <73979892+AshleyWhaley@users.noreply.github.com>
2021-06-01 18:06:39 -0700

    added profile page to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Footer from './components/Footer';
 import Nav from './components/Nav';
 import Login from './pages/Login/Login'
 import Signup from './pages/SignUp/Signup'
+import Saved from './pages/Profile/Profile'
 
 function App() {
     document.title = "Take A Hike"
