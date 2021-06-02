4c4c93b6bbff49f6a5e80ba380d8370f625d25ce
Nate J <jamis.walsh@gmail.com>
2021-06-01 18:58:15 -0700

    quick push

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -43,7 +43,7 @@ export function SearchBar(props) {
     return (
       <div className="wrap">
         <div className="uk-flex">
-        <div className="uk-card uk-card-default uk-card-hover uk-width-1-5@m uk-margin-left">
+        <div className="uk-card uk-card-default uk-card-hover uk-width-1-4@m uk-margin-left">
           <h3 className="uk-card-title">Search For Hike</h3>
         <form className="uk-search uk-search-default" onSubmit={handleSubmit}>
             <div className="search-bar-component">
@@ -63,7 +63,7 @@ export function SearchBar(props) {
               </div>
             </form>
           </div>
-          <div className="uk-card uk-card-default uk-card-hover uk-width-1-4@m uk-margin-left">
+          <div className="uk-card uk-card-default uk-card-hover uk-width-expand uk-margin-left uk-margin-right">
             <div className="search-result">
               <h2>
                 Trail Name: {trailResults[0].name}
