172bb12e868b76795f2457d9357d372ead8e20af
zackjj <zack.j.johnston@gmail.com>
2021-06-01 18:22:55 -0700

    refactored to have state at the global level, accessible, changeable from different pages

--- client/src/components/SearchResults/index.js
+++ client/src/components/SearchResults/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./style.css";
 
-function SearchResults() {
+function SearchResults(props) {
   return (
     <div className="uk-card uk-card-default uk-card-hover uk-width-1-4@m uk-margin-left">
     <div className="search-result">
-        <h2>Trail Name: {JSON.stringify(trailResults[0].name)}</h2>
-        <p>Location: {JSON.stringify([trailResults[0].location.city, trailResults.location.state])}</p>
+        <img src={props.trailResults.image_url}></img>
     </div>
   </div>
   );
